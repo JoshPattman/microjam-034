268102f2a0194a7c96e3336b84cfbb3740c4baf3
Josh Pattman <Josh.pattman@gmail.com>
2025-02-22 20:21:03 +0000

    more enemies

--- godot/scripts/game.gd
+++ godot/scripts/game.gd
@@ -7,6 +7,12 @@ var time_since_last_asteroid: float = 0.0
 
 var resource_asteroid_scene = preload("res://game_objects/resource_asteroid.tscn")
 
+@export var enemy_spawn_groups: Array[SpawnGroup]
+@export var enemy_frequency: float = 1.0
+@export var max_enemies = 10
+@export var enemy_spawn_radius: float = 50
+var time_since_last_enemy: float = 0.0
+
 var player_resources: float = 0.0:
 	set(new):
 		$PlayerCamera/UI/Resources/Label.text = str(new)
@@ -31,6 +37,20 @@ func spawn_asteroid():
 	$Asteroids.add_child(instance)
 	instance.global_position = _get_random_asteroid_spawn_loc()
 
+func spawn_enemy():
+	if len(get_tree().get_nodes_in_group("enemies")) > max_enemies:
+		return
+	var group = enemy_spawn_groups[randi_range(0, len(enemy_spawn_groups)-1)]
+	var loc = _get_random_asteroid_spawn_loc()
+	var angle: float = 0.0
+	for elem: PackedScene in group.items:
+		var ins = elem.instantiate()
+		add_child(ins)
+		if ins is Node2D:
+			ins.global_position = loc + Vector2(1, 0).rotated(angle)
+		angle += PI * 2 / len(group.items)
+		print("Spawned ", ins.name)
+
 func spawn_resource_asteroid():
 	var instance = resource_asteroid_scene.instantiate()
 	$Asteroids.add_child(instance)
@@ -41,3 +61,8 @@ func _process(delta: float) -> void:
 	if time_since_last_asteroid > asteroid_frequency:
 		spawn_resource_asteroid()
 		time_since_last_asteroid = 0.0
+	
+	time_since_last_enemy += delta * CustomRigidbody2D.get_global_dt_mult()
+	if time_since_last_enemy > enemy_frequency:
+		spawn_enemy()
+		time_since_last_enemy = 0
