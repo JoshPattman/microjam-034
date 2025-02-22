6004f2fb253fceb4cd31f4134d2bad865bd92a24
Josh Pattman <Josh.pattman@gmail.com>
2025-02-22 14:58:04 +0000

    time dialation and black holes

--- godot/scripts/time_dialater.gd
+++ godot/scripts/time_dialater.gd
@@ -2,8 +2,11 @@ extends Node2D
 
 class_name TimeDialater
 
-@export var multiplier = 0.5
-@export var range = 100
+@export var slow_mult = 0.5
+@export var slow_range = 100
+
+@export var pull_force: float = 400.0
+@export var pull_range: float = 600.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,7 +19,14 @@ func _process(delta: float) -> void:
 
 func get_multiplier_at(pos: Vector2) -> float:
 	var dist = (position-pos).length()
-	if dist <= range:
-		return multiplier
-	else:
+	if dist > slow_range:
 		return 1.0
+	var t = 1 - (dist / slow_range)
+	return (1-t) * 1.0 + t * slow_mult
+
+func get_acceleration_at(pos: Vector2) -> Vector2:
+	var delta = (position-pos)
+	var dist = delta.length()
+	if dist > pull_range:
+		return Vector2()
+	return (delta / dist) * (1 - (dist/pull_range)) * pull_force
