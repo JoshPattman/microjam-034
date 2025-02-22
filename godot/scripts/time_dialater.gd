2c08df0e3251e1bbbec926e34976d56fc7dc1879
Josh Pattman <Josh.pattman@gmail.com>
2025-02-22 11:21:52 +0000

    Some time dialation

---
+++ godot/scripts/time_dialater.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+class_name TimeDialater
+
+@export var multiplier = 0.5
+@export var range = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	add_to_group("time_dialaters")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func get_multiplier_at(pos: Vector2) -> float:
+	var dist = (position-pos).length()
+	if dist <= range:
+		return multiplier
+	else:
+		return 1.0
