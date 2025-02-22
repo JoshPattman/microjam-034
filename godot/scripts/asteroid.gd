aa64bd73697e6e28fcae55fc5952b526bfd8be4d
jackdarlison <jdarlison7@gmail.com>
2025-02-22 14:10:01 +0000

    ship

--- godot/scripts/asteroid.gd
+++ godot/scripts/asteroid.gd
@@ -1,6 +1,6 @@
 class_name Asteroid extends CustomRigidbody2D
 
-@export var speed: float
+@export var speed: float = 1.0
 
 func _ready() -> void:
 	real_velocity = Utils.get_random_unit_vector() * speed
