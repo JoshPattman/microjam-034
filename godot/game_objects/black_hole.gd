eecf4a1ab1771d7b459d14989bde4e2950544d67
Josh Pattman <Josh.pattman@gmail.com>
2025-02-22 19:44:30 +0000

    spawn asteroids around player

--- godot/game_objects/black_hole.gd
+++ godot/game_objects/black_hole.gd
@@ -1,10 +1,16 @@
 extends Area2D
 
+@export var allow_suck: bool = true
+@export var add_to_asteroid_spawner: bool = false
+
 func _ready() -> void:
+	if add_to_asteroid_spawner:
+		add_to_group("asteroid_spawner")
 	connect("body_entered", on_body_entered)
 
 func on_body_entered(body: Node2D):
-	if body is CustomRigidbody2D:
-		body.enter_blackhole(global_position)
-	else:
-		queue_free()
+	if allow_suck:
+		if body is CustomRigidbody2D:
+			body.enter_blackhole(global_position)
+		else:
+			body.queue_free()
