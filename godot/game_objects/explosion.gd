ca0f29b22b0ac29957b94103ae00f66d00aee1d3
Josh Pattman <Josh.pattman@gmail.com>
2025-02-22 22:34:01 +0000

    boom

--- godot/game_objects/explosion.gd
+++ godot/game_objects/explosion.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
 func _ready() -> void:
-	$AnimatedSprite2D.animation_finished.connect(queue_free)
+	$AnimatedSprite2D.animation_finished.connect(finish)
 	$AnimatedSprite2D.play()
+	
+	$AudioStreamPlayer.finished.connect(finish)
+
+var num_finish: int = 0
+
+func finish() -> void:
+	num_finish += 1
+	if num_finish == 2:
+		queue_free()
