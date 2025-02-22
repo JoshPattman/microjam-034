41692d4b522183d869604d75c3471f4578b543be
Josh Pattman <Josh.pattman@gmail.com>
2025-02-22 11:56:21 +0000

    Physics

--- godot/scripts/custom_rb.gd
+++ godot/scripts/custom_rb.gd
@@ -2,14 +2,29 @@ extends CharacterBody2D
 
 class_name CustomRigidbody2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+var real_velocity: Vector2 = Vector2()
+var real_angular_velocity: float = 0.0
+
+var _total_real_force: Vector2 = Vector2()
+var _total_real_torque: float = 0.0
+
+# Override this to do physics
+func _custom_physics_process(delta: float) -> void:
 	pass
 
-
 func _physics_process(delta: float) -> void:
-	velocity = Vector2(500, 0)
+	var tm = calculate_time_multiplier()
+	_custom_physics_process(delta*tm)
+	var real_acceleration = _total_real_force * delta
+	real_velocity += real_acceleration * delta
+	velocity = real_velocity * tm
+	var real_angular_acceleration = _total_real_torque * delta
+	real_angular_velocity += real_angular_acceleration * delta
+	var angular_velocity = real_angular_velocity * tm
+	rotation += angular_velocity * delta
 	move_and_slide()
+	_total_real_force = Vector2()
+	_total_real_torque = 0
 
 
 func calculate_time_multiplier() -> float:
@@ -19,3 +34,9 @@ func calculate_time_multiplier() -> float:
 		if node is TimeDialater:
 			total_muliplier *= node.get_multiplier_at(position)
 	return total_muliplier
+
+func add_force(force: Vector2) -> void:
+	_total_real_force += force
+
+func add_torque(torque: float) -> void:
+	_total_real_torque += torque
