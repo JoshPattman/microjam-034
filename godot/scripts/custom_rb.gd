20f8ff66d1e514f1af399080aa6c91b608fd37f6
Josh Pattman <Josh.pattman@gmail.com>
2025-02-22 11:21:52 +0000

    Some time dialation

---
+++ godot/scripts/custom_rb.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+class_name CustomRigidbody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	pass
+
+func calculate_time_multiplier() -> float:
+	var tds = get_tree().get_nodes_in_group("time_dialaters")
+	var total_muliplier = 1.0
+	for node in tds:
+		if node is TimeDialater:
+			total_muliplier *= node.get_multiplier_at(position)
+	return total_muliplier
