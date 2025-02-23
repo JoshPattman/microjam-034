c5c4a0624641d7acdb8e983ca0f1a1f57bd6c677
Josh Pattman <Josh.pattman@gmail.com>
2025-02-23 11:11:49 +0000

    Pusher works

--- godot/scripts/turret.gd
+++ godot/scripts/turret.gd
@@ -2,12 +2,20 @@ extends Node2D
 
 class_name Turret
 
+@export_category("Turret")
 @export var range: float = 200
 @export var shot_delay: float = 1.0
+@export var is_single_shot: bool = true
+@export var is_pusher: bool = false
+@export var push_prefab: PackedScene
+@export var push_for: float = 3.0
 
+@export_category("Enemies")
 @export var add_to_targets: bool = true
 
 var time_since_last_shot: float = 0.0
+var pushing_time_left: float = 0.0
+var current_push_prefab: Node2D
 
 func _ready() -> void:
 	if add_to_targets:
@@ -23,8 +31,28 @@ func _process(delta: float) -> void:
 			if dist < closest_dist_squared:
 				closest_dist_squared = dist
 				closest_enemy = e
+	
 	if closest_dist_squared < range*range:
 		if time_since_last_shot > shot_delay:
 			time_since_last_shot = 0.0
-			closest_enemy.blow_up()
-	time_since_last_shot += delta * CustomRigidbody2D.get_global_dt_mult()
+			if is_single_shot:
+				closest_enemy.blow_up()
+			elif is_pusher:
+				pushing_time_left = push_for
+				var pu = push_prefab.instantiate()
+				print("create")
+				if pu is Node2D:
+					pu.global_position = global_position
+					if current_push_prefab != null:
+						current_push_prefab.queue_free()
+					current_push_prefab = pu
+					add_child(current_push_prefab)
+			else:
+				print("useless turret")
+	
+	if current_push_prefab != null && pushing_time_left <= 0:
+		current_push_prefab.queue_free()
+	
+	var rdelta = delta * CustomRigidbody2D.get_global_dt_mult()
+	time_since_last_shot += rdelta
+	pushing_time_left -= rdelta
