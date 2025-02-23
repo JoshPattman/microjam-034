f11da6fd5a49826c6d0ea4c2e0f8b50f4b4600d8
Josh Pattman <Josh.pattman@gmail.com>
2025-02-23 19:14:50 +0000

    game over

---
+++ godot/scripts/game_over.gd
@@ -0,0 +1,10 @@
+extends Control
+
+@export var game_scene: PackedScene
+@export var menu_scene: PackedScene
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("game_restart"):
+		get_tree().change_scene_to_packed(game_scene)
+	if Input.is_action_just_pressed("game_quit"):
+		get_tree().change_scene_to_packed(menu_scene)
