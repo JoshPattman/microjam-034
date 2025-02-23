4f207a26f5a5cd16fc60c2c3f7230a7634e7039d
Josh Pattman <Josh.pattman@gmail.com>
2025-02-23 14:48:22 +0000

    orbiting ships

--- godot/scripts/enemy.gd
+++ godot/scripts/enemy.gd
@@ -20,6 +20,12 @@ var target_direction: Vector2
 @export var kamikazee_explosion: PackedScene
 @export var kamikazee_damage: int = 1
 
+@export var orbit_dist: float = 300
+
+@export var spawn_ship: PackedScene
+@export var spawn_delay: float = 2.0
+var spawn_timer: float = 0.0
+
 var things_to_avoid: Array[EnemyAvoid] = []
 var enemies_in_range: Array[Enemy] = []
 var recalc_avoids_timer: float = 0.0
@@ -29,6 +35,8 @@ func _on_rb_collision(point: Vector2, normal: Vector2, other: CustomRigidbody2D)
 	var other_life = Life.get_life_script(other)
 	if other_life != null:
 		other_life.damage(kamikazee_damage)
+	if other is Asteroid:
+		real_velocity += normal * 75
 
 func _ready() -> void:
 	add_to_group("enemies")
@@ -45,7 +53,15 @@ func _custom_process(delta: float) -> void:
 	
 	var d_target: Vector2
 	if current_target != null:
-		d_target = (current_target.global_position - global_position).normalized()
+		if is_kamikazee:
+			d_target = (current_target.global_position - global_position).normalized()
+		else:
+			var target_delta = current_target.global_position - global_position
+			# T is 0 when close and 1 when far
+			var t = tanh((target_delta.length() - orbit_dist) / 100)/2+0.5
+			var target_norm_delta = target_delta.normalized()
+			var target_orbit_delta = target_norm_delta.rotated(PI/2)
+			d_target = ((1-t)*target_orbit_delta + t*target_norm_delta).normalized()
 	
 	if recalc_avoids_timer >= recalc_avoids_every:
 		var avoids = get_tree().get_nodes_in_group("enemy_avoid")
@@ -92,10 +108,18 @@ func _custom_process(delta: float) -> void:
 				var lc = Life.get_life_script(current_target)
 				if lc != null:
 					lc.damage(kamikazee_damage)
+		else:
+			if spawn_timer > spawn_delay:
+				spawn_timer = 0
+				var ins = spawn_ship.instantiate()
+				if ins is Node2D:
+					ins.global_position = global_position - current_forward()*30
+				add_sibling(ins)
 	
 	target_direction = d_target*k_targeting + d_avoid*k_avoiding + d_separate*k_separate
 	if target_direction.length() > 1:
 		target_direction = target_direction.normalized()
+	spawn_timer += delta * CustomRigidbody2D.get_global_dt_mult()
 
 func blow_up():
 	var exp = kamikazee_explosion.instantiate()
