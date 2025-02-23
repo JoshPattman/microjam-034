f9a676b84558a6efed244805290d330fa549b53c
Josh Pattman <Josh.pattman@gmail.com>
2025-02-23 11:28:58 +0000

    asteroids amount of resources reflect their size

--- godot/scripts/resource_asteroid.gd
+++ godot/scripts/resource_asteroid.gd
@@ -6,7 +6,7 @@ var amount: float
 
 func _ready() -> void:
 	super._ready()
-	amount = start_amount
+	reset_resources()
 
 # returns  [is_dead: bool, mined_amount: float]
 func mine(to_mine: float) -> Array:
@@ -20,3 +20,8 @@ func mine(to_mine: float) -> Array:
 		queue_free()
 	
 	return [is_equal_approx(amount, 0.0), mine_amount]
+
+func reset_resources() -> void:
+	amount = round(start_amount * scale.x * scale.y)
+	if amount < 1:
+		amount = 1
